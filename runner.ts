fc92a079e9234a9688ac657f45d467c493ce9409
nsx07 <felipeaugustodluz@hotmail.com>
2024-03-31 19:34:59 -0300

    removing wrong located generated code

--- runner.ts
+++ runner.ts
@@ -1,10 +1,7 @@
 import { CharStreams, CommonTokenStream } from "antlr4ts";
-import { Expr_listContext, calcParser } from "./generated/calcParser";
+import { calcParser } from "./generated/calcParser";
 import { calcLexer } from "./generated/calcLexer";
 import * as fs from "fs";
-import { calcListener } from "./calcListener";
-import { Func_Context } from "./calcParser";
-import { ParseTreeWalker } from "antlr4ts/tree";
 
 // Create the lexer and parser
 const code = fs.readFileSync("EXAMPLE.txt", "utf8");
@@ -13,16 +10,3 @@ const lexer = new calcLexer(inputStream);
 const tokenStream = new CommonTokenStream(lexer);
 const parser = new calcParser(tokenStream);
 const tree = parser.equation();
-
-class ErrorCalcListener implements calcListener {
-  constructor() {}
-  enterFunc_(ctx: Func_Context) {
-    console.log("Function: ", ctx._start.text);
-  }
-  enterExpr_list(ctx: Expr_listContext) {
-    console.log("Expr_list: ", ctx._start.text);
-  }
-}
-
-const listener: calcListener = new ErrorCalcListener();
-ParseTreeWalker.DEFAULT.walk(listener, tree);
